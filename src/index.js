ecfc150d0549d8391b268c44c353050ef539818c
Natein <natein@gmail.com>
2020-02-21 00:00:16 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+    let res = "";
+    let zero_to_nine = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let ten_to_nineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let ten_to_hundred = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if(number >= 100){
+        let hun = Math.trunc(number/100);
+        res = zero_to_nine[hun] + " hundred ";
+        number = number % 100;
+    }
+    if(number >= 10) {
+        if(number >= 10 && number <= 19) res = res + ten_to_nineteen[number%10];
+        else {
+            let dec = Math.trunc(number/10);
+            res = res + ten_to_hundred[dec-2] + " ";
+            if(number%10 != 0) res = res + zero_to_nine[number%10];
+        }
+    }
+    else {
+        if(number == 0 && res.length != 0) {}
+        else res = res + zero_to_nine[number];
+    }
+    return res;  
 }
