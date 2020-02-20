b2092ff230772a4ff17387de71676fb6f6e02bb6
Natein <natein@gmail.com>
2020-02-21 01:31:25 +0200

    fix: whitespace mistake

--- src/index.js
+++ src/index.js
@@ -5,20 +5,30 @@ module.exports = function toReadable (number) {
     let ten_to_hundred = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     if(number >= 100){
         let hun = Math.trunc(number/100);
-        res = zero_to_nine[hun] + " hundred ";
+        res = zero_to_nine[hun] + " hundred";
         number = number % 100;
     }
-    if(number >= 10) {
-        if(number >= 10 && number <= 19) res = res + ten_to_nineteen[number%10];
+    if(number >= 10) {        
+        if(number >= 10 && number <= 19) {
+            if(res.length != 0) res = res + " ";
+            res = res + ten_to_nineteen[number%10];
+        }
         else {
             let dec = Math.trunc(number/10);
-            res = res + ten_to_hundred[dec-2] + " ";
-            if(number%10 != 0) res = res + zero_to_nine[number%10];
+            if(res.length != 0) res = res + " ";
+            res = res + ten_to_hundred[dec-2];
+            if(number%10 != 0) {
+                if(res.length != 0) res = res + " ";
+                res = res + zero_to_nine[number%10];
+            }
         }
     }
     else {
         if(number == 0 && res.length != 0) {}
-        else res = res + zero_to_nine[number];
+        else {
+            if(res.length != 0) res = res + " ";
+            res = res + zero_to_nine[number];
+        }
     }
-    return res;  
+    return res;
 }
